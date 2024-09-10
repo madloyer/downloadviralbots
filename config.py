86bf601e571abe96b94dabb07d0ba874b60ef76f
madloyer <87596107+madloyer@users.noreply.github.com>
2024-09-11 06:43:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,16 +11,16 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7381900258:AAF3Wz4C_aavtAWB1WbOw88oVaSnf2Z7fUw")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 # API ID Anda dari my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "11450835"))
+APP_ID = int(os.environ.get("APP_ID", " "))
 
 # API Hash Anda dari my.telegram.org
-API_HASH = os.environ.get("API_HASH", "0fadb61feae6ccf016932823bbf1565c")
+API_HASH = os.environ.get("API_HASH", " ")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002495175733"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-100"))
 
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "mantapvids")
@@ -36,10 +36,10 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "postgres://ocwkivvr:IsjzOpqcvahaM1t5nwJyTBCOmL1IXL7t@satao.db.elephantsql.com/ocwkivvr")
+DB_URI = os.environ.get("DATABASE_URL", " ")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001755788957"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
@@ -50,7 +50,7 @@ START_MSG = os.environ.get(
     "<b>Hello {first}</b>\n\n<b>Saya dapat menyimpan file pribadi di Channel Tertentu dan pengguna lain dapat mengaksesnya dari link khusus.</b>",
 )
 try:
-    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "1281619082").split())]
+    ADMINS = [int(x) for x in (os.environ.get("ADMINS", " ").split())]
 except ValueError:
     raise Exception("Daftar Admin Anda tidak berisi User ID Telegram yang valid.")
 
