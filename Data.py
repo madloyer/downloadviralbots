06dcfa724bdc4a0ed7c521eb90ef4253d2b19ed9
madloyer <87596107+madloyer@users.noreply.github.com>
2024-09-22 01:37:36 +0700

    Update Data.py

--- Data.py
+++ Data.py
@@ -23,7 +23,7 @@ class Data:
  ├ /speedtest - Untuk Mengetes kecepatan server bot
  └ /broadcast - Untuk mengirim pesan broadcast ke pengguna bot
 
-👨‍💻 Develoved by </b><a href='https://t.me/zioassistant'> @zioassistant </a>
+👨‍💻 Develoved by </b><a href='https://t.me/TiktokViralKini'> @TiktokViralKini </a>
 """
 
     close = [
@@ -53,5 +53,5 @@ class Data:
  • Framework: <a href='https://docs.pyrogram.org'>Pyrogram</a>
  • Source Code: <a href='https://github.com'</a>
 
-👨‍💻 Develoved by </b><a href='https://t.me/zioassistan't>@zioassistant</a>
+👨‍💻 Develoved by </b><a href='https://t.me/Mantapvids't>@Mantapvids</a>
 """
