ed4059824215c41aa5ffc0a7b4dec677b2934fa5
madloyer <87596107+madloyer@users.noreply.github.com>
2024-11-15 01:11:12 +0700

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://rxgnirzc:qGW2PM472OHG6GStp7zSm8C_XuxcPrdO@lucky.db.elephantsql.com/rxgnirzc")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002316739161"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001755788957"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
